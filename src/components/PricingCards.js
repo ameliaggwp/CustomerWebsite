53acf7757541d9645effdb64a1bc6b5fb29fc50e
unknown <ameliaggwp@gmail.com>
2020-04-22 18:02:58 -0700

    Adding style to pricing cards

--- src/components/PricingCards.js
+++ src/components/PricingCards.js
@@ -1,33 +1,65 @@
 import React from "react";
+import chatIcon from "./assets/chat-quote-fill.svg";
+import peopleIcon from "./assets/people-fill.svg";
+import fileIcon from "./assets/file-richtext.svg";
 
-const Card = ({ price, service, description }) => {
+const Card = ({ icon, price, service, description }) => {
   return (
     <div className="col-sm">
-      <div className="pricing-card price">{price}</div>
-      <div className="pricing-card service-card">{service}</div>
-      <div className="pricing-card">{description}</div>
+      <div className="card text-center">
+        <div className="card-body">
+          <div className="pricing-card icon">
+            <img className="card-icon" src={icon} alt={icon} />
+          </div>
+          <h5 className="card-title">
+            <div className="pricing-card service-card">{service}</div>
+          </h5>
+          <div className="card-text text-center">{description}</div>
+          <div className="pricing-card price">{price}</div>
+        </div>
+      </div>
     </div>
   );
 };
+
 //Intended for use with 3 card components
 const PricingCards = () => {
   return (
     <div className="container">
-      <div className="row">
+      <div className="row align-equal row-padding">
         <Card
-          price="15$"
+          icon={fileIcon}
+          price="15$/flat-rate"
           service="Worksheet"
-          description=" This is a description"
+          description={
+            <div className="description">
+              <div className="bullet-item">PDF lesson worksheets</div>
+              <div className="bullet-item">Dialogue videos</div>
+            </div>
+          }
         />
         <Card
-          price="30$"
+          icon={chatIcon}
+          price="30$/30 mins"
           service="Conversation Practice"
-          description=" This is a description"
+          description={
+            <div className="description">
+              <div className="bullet-item">Video-chat session</div>
+              <div className="bullet-item">Journal review</div>
+            </div>
+          }
         />
         <Card
-          price="60$"
+          icon={peopleIcon}
+          price="60$/1 hr"
           service="Personal Tutoring"
-          description=" This is a description"
+          description={
+            <div className="description">
+              <div className="bullet-item">PDF lesson worksheet</div>
+              <div className="bullet-item">Video-chat session</div>
+              <div className="bullet-item">Formulated individual lesson</div>
+            </div>
+          }
         />
       </div>
     </div>
