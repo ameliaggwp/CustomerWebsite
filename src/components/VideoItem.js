b7e6670ed30c911dd2f89c6a5da231e1dd54756e
unknown <ameliaggwp@gmail.com>
2020-05-04 16:31:12 -0700

    Changing VideoItem to class component and initializing state

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,16 +1,41 @@
 import React from "react";
 
-const VideoItem = ({ video, onVideoSelect }) => {
-  return (
-    <div onClick={() => onVideoSelect(video)} className="col-6 video-item">
-      <img
-        src={video.snippet.thumbnails.high.url}
-        className="card-img-top"
-        alt={video.snippet.title}
-      />
-      <div className="video-card title">{video.snippet.title}</div>
-    </div>
-  );
+class VideoItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isHidden: true,
+    };
+  }
+
+  toggleHidden() {
+    this.setState({ isHidden: !this.state.isHidden });
+  }
+
+  render() {
+    return (
+      <div
+        onClick={() => this.props.onVideoSelect(this.props.video)}
+        className="col-6 video-item"
+        onMouseEnter={this.toggleHidden.bind(this)}
+        onMouseLeave={this.toggleHidden.bind(this)}
+      >
+        <img
+          src={this.props.video.snippet.thumbnails.high.url}
+          className="card-img-top"
+          alt={this.props.video.snippet.title}
+        />
+        {!this.state.isHidden && (
+          <Child title={this.props.video.snippet.title} />
+        )}
+      </div>
+    );
+  }
+}
+
+const Child = ({ title }) => {
+  return <div className="video-card title">{title}</div>;
 };
 
 export default VideoItem;
