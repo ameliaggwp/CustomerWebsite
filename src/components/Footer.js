a3ff8fef239af3db7ae4806f98e1d1b13688db0d
unknown <ameliaggwp@gmail.com>
2020-05-06 11:57:58 -0700

    Styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,33 +7,67 @@ import Youtube from "./assets/yt_icon_mono_dark.png";
 
 const Footer = () => {
   return (
-    <footer>
-      <a href="#Top">Top^</a>
-      <div className="container-fluid">
-        <div className="social-follow">
-          <a href="https://twitter.com/abbyjpee">
-            <img className="social-icon" src={Twitter} alt="twitter" />
-          </a>
-          <a href="https://www.facebook.com/abbypeeofficial">
-            <img className="social-icon" src={Facebook} alt="facebook" />
-          </a>
-          <a href="https://www.twitch.tv/abbyp">
-            <img className="social-icon" src={Twitch} alt="twitch" />
-          </a>
-          <a href="https://www.youtube.com/channel/UCHEEOe5SnxYau9puXKtqx5Q">
-            <img className="social-icon" src={Youtube} alt="youtube" />
-          </a>
-          <a href="https://www.instagram.com/abbyjpee/">
-            <img
-              className="social-icon"
-              id="invert"
-              src={Instagram}
-              alt="instagram"
-            />
-          </a>
-        </div>
-      </div>
-    </footer>
+    <React.Fragment>
+      <div className="container"></div>
+      <footer>
+        {/* Footer Main */}
+        <section className="ft-main">
+          <div className="ft-main-item">
+            This is where the text bubble will be col 1
+          </div>
+          <div className="ft-main-item">
+            This is where the picture of Abby will be col 2
+          </div>
+        </section>
+        {/* Footer Main */}
+        <section className="ft-social">
+          <ul className="ft-social-list">
+            <li>
+              <a href="https://twitter.com/abbyjpee">
+                <img className="social-icon" src={Twitter} alt="twitter" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/abbypeeofficial">
+                <img className="social-icon" src={Facebook} alt="facebook" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitch.tv/abbyp">
+                <img className="social-icon" src={Twitch} alt="twitch" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/channel/UCHEEOe5SnxYau9puXKtqx5Q">
+                <img className="social-icon" src={Youtube} alt="youtube" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/abbyjpee/">
+                <img
+                  className="social-icon"
+                  id="invert"
+                  src={Instagram}
+                  alt="instagram"
+                />
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        {/* Footer credits */}
+        <section className="ft-credits">
+          <ul className="ft-credits-list">
+            <li>
+              <a href="#Top">Top^</a>
+            </li>
+            <li>
+              <p>Website design by Amelia Dominguez</p>
+            </li>
+          </ul>
+        </section>
+      </footer>
+    </React.Fragment>
   );
 };
 export default Footer;
